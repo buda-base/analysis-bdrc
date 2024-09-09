72b1c86f8f6a688819021117d4b449fc7db7bb79
eroux <elie.roux@telecom-bretagne.eu>
2024-09-09 14:24:21 +0200

    implement Sa analyzers

--- opensearch/src/main/java/io/bdrc/opensearch/BDRCAnalyzersPlugin.java
+++ opensearch/src/main/java/io/bdrc/opensearch/BDRCAnalyzersPlugin.java
@@ -1,12 +1,8 @@
 package io.bdrc.opensearch;
 
-import org.apache.lucene.analysis.Analyzer;
-import org.opensearch.index.analysis.AnalyzerProvider;
 import org.opensearch.index.analysis.CharFilterFactory;
-import org.opensearch.index.analysis.TokenFilterFactory;
 import org.opensearch.index.analysis.TokenizerFactory;
 import org.opensearch.indices.analysis.AnalysisModule;
-import org.opensearch.indices.analysis.AnalysisModule.AnalysisProvider;
 import org.opensearch.plugins.AnalysisPlugin;
 import org.opensearch.plugins.Plugin;
 
@@ -18,5 +14,17 @@ import java.util.Map;
  */
 public class BDRCAnalyzersPlugin extends Plugin implements AnalysisPlugin {
 
+    @Override
+    public Map<String, AnalysisModule.AnalysisProvider<CharFilterFactory>> getCharFilters() {
+        Map<String, AnalysisModule.AnalysisProvider<CharFilterFactory>> extra = new HashMap<>();
+        extra.put("sanskrit", SanskritCharFilterFactory::new);
+        return extra;
+    }
     
+    @Override
+    public Map<String, AnalysisModule.AnalysisProvider<TokenizerFactory>> getTokenizers() {
+        Map<String, AnalysisModule.AnalysisProvider<TokenizerFactory>> extra = new HashMap<>();
+        extra.put("sanskrit", SanskritTokenizerFactory::new);
+        return extra;
+    }
 }
